cde331bc472265e60282617b3a9b32935243da5b
Yoga Agung Prasetio <yoga.agung@transtrack.id>
2023-11-10 10:35:49 +0700

    initial commit

---
+++ src/Listener.js
@@ -0,0 +1,31 @@
+class Listener {
+  constructor(playlistsService, mailSender) {
+    this._playlistsService = playlistsService;
+    this._mailSender = mailSender;
+
+    this.listen = this.listen.bind(this);
+  }
+
+  async listen(message) {
+    try {
+      const { playlistId, targetEmail } = JSON.parse(
+        message.content.toString(),
+      );
+
+      const playlist = await this._playlistsService.getPlaylists(
+        playlistId,
+      );
+      const result = await this._mailSender.sendEmail(
+        targetEmail,
+        playlistId,
+        playlist.name,
+        JSON.stringify({ playlist }),
+      );
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+module.exports = Listener;
